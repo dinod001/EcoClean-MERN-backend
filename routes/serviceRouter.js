0855124482236f3f7d778af9bc5340f5ef5ad756
Heshan Asmadala <Heshanasmadala@gmail.com>
2025-06-28 15:44:00 +0530

    service router aunthetication updated

--- routes/serviceRouter.js
+++ routes/serviceRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { addService, deleteService, getAllServices, getServiceById, updateService } from "../controllers/serviceController.js";
 import upload from "../config/multer.js";
+import { personnelAuthentication } from "../middleware/personnelAuthMiddleware.js";
 
 const serviceRouter=express.Router()
 
 
-serviceRouter.post("/add-new-service",upload.single("image"),addService)
+serviceRouter.post("/add-new-service",personnelAuthentication,upload.single("image"),addService)
 serviceRouter.get("/get-service/:id",getServiceById)
 serviceRouter.get("/get-all-services",getAllServices)
-serviceRouter.delete("/delete-service/:id",deleteService)
-serviceRouter.patch("/update-service/:id",upload.single("image"),updateService)
+serviceRouter.delete("/delete-service/:id",personnelAuthentication,deleteService)
+serviceRouter.patch("/update-service/:id",personnelAuthentication,upload.single("image"),updateService)
 
 export default serviceRouter
